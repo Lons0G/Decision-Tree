bc531107d35231aea4e906402a54626dc765cb42
Angel A. Galarza <angel_alonso1@outlook.es>
2024-11-04 02:28:07 -0600

    Update: Adult-Income & Cancer Dataset

--- outputs/rules/rules.py
+++ outputs/rules/rules.py
@@ -1,38 +1,32 @@
-def findDecision(obj): #obj[0]: sepal_length, obj[1]: sepal_width, obj[2]: petal_length, obj[3]: petal_width
-   # {"feature": "petal_length", "instances": 105, "metric_value": 1.5822, "depth": 1}
-   if obj[2]>1.9431970926890703:
-      # {"feature": "petal_width", "instances": 68, "metric_value": 0.9975, "depth": 2}
-      if obj[3]<=1.7:
-         # {"feature": "sepal_length", "instances": 35, "metric_value": 0.5127, "depth": 3}
-         if obj[0]<=7.0:
-            # {"feature": "sepal_width", "instances": 34, "metric_value": 0.4306, "depth": 4}
-            if obj[1]<=2.8:
-               return 'Iris-versicolor'
-            elif obj[1]>2.8:
-               return 'Iris-versicolor'
+def findDecision(obj): #obj[0]: 0, obj[1]: 1, obj[2]: 2, obj[3]: 3, obj[4]: 4, obj[5]: 5, obj[6]: 6
+   # {"feature": 0, "instances": 398, "metric_value": 0.9877, "depth": 1}
+   if obj[0]>-5.565735898981081:
+      # {"feature": 1, "instances": 397, "metric_value": 0.9871, "depth": 2}
+      if obj[1]>-7.77585280424806:
+         # {"feature": 2, "instances": 396, "metric_value": 0.9865, "depth": 3}
+         if obj[2]>-4.34191581326578:
+            # {"feature": 5, "instances": 395, "metric_value": 0.986, "depth": 4}
+            if obj[5]>-3.511878193549121:
+               # {"feature": 3, "instances": 394, "metric_value": 0.9854, "depth": 5}
+               if obj[3]<=4.240777195860392:
+                  return 'B'
+               elif obj[3]>4.240777195860392:
+                  return 'B'
+               else:
+                  return 'B'
+            elif obj[5]<=-3.511878193549121:
+               return 'M'
             else:
-               return 'Iris-versicolor'
-         elif obj[0]>7.0:
-            return 'Iris-virginica'
+               return 'B'
+         elif obj[2]<=-4.34191581326578:
+            return 'M'
          else:
-            return 'Iris-versicolor'
-      elif obj[3]>1.7:
-         # {"feature": "sepal_length", "instances": 33, "metric_value": 0.1959, "depth": 3}
-         if obj[0]>5.9:
-            return 'Iris-virginica'
-         elif obj[0]<=5.9:
-            # {"feature": "sepal_width", "instances": 6, "metric_value": 0.65, "depth": 4}
-            if obj[1]<=3.0:
-               return 'Iris-virginica'
-            elif obj[1]>3.0:
-               return 'Iris-versicolor'
-            else:
-               return 'Iris-virginica'
-         else:
-            return 'Iris-virginica'
+            return 'B'
+      elif obj[1]<=-7.77585280424806:
+         return 'M'
       else:
-         return 'Iris-virginica'
-   elif obj[2]<=1.9431970926890703:
-      return 'Iris-setosa'
+         return 'B'
+   elif obj[0]<=-5.565735898981081:
+      return 'M'
    else:
-      return 'Iris-setosa'
+      return 'B'
